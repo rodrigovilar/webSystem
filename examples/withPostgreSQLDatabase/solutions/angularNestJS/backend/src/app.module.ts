f2a16459c70a32bbe493559cb9369a767fbb435d
Vinnicius S <niknicius@gmail.com>
2020-01-23 11:14:48 -0300

    Writing solution backend with PostgreSQL database using Nest.js

---
+++ examples/withPostgreSQLDatabase/solutions/angularNestJS/backend/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {GraphQLModule} from '@nestjs/graphql';
+import {BookHttpModule} from './book/book-http.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [GraphQLModule.forRoot({
+              autoSchemaFile: 'schema.gql',
+              playground: true,
+              debug: false,
+            }),
+            BookHttpModule,
+            DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ examples/withPostgreSQLDatabase/solutions/angularNestJS/backend/src/book/book.service.ts
@@ -0,0 +1,30 @@
+import {Inject, Injectable} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Book} from './model/book.entity';
+import {DeleteResult, InsertResult, Repository, UpdateResult} from 'typeorm';
+
+@Injectable()
+export class BookService {
+    constructor(@Inject('BOOKS_REPOSITORY') private readonly bookRepository: typeof Book) {}
+
+    findAll(): Promise<Book[]> {
+        return this.bookRepository.findAll<Book>();
+    }
+
+    find(id): Promise<Book> {
+       return this.bookRepository.findOne({where: {id}});
+    }
+
+    create(data): Promise<Book> {
+        return data.save();
+    }
+
+    update(data): Promise<Book> {
+        this.bookRepository.update<Book>(data, { where: {id: data.id}, returning: true  });
+        return this.bookRepository.findOne({where: {id: data.id}});
+    }
+
+    delete(id): Promise<number> {
+        return this.bookRepository.destroy({where: {id} });
+    }
+}
