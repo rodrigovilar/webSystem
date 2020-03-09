f2ef56b07c50ddf2ec969074ed17c2990796c65e
Vinnicius S <niknicius@gmail.com>
2020-03-09 15:20:12 -0300

    Resolver Cleaned

--- examples/withPostgreSQLDatabase/solutions/angularNestJSLambda/backend/src/book/book.resolver.ts
+++ examples/withPostgreSQLDatabase/solutions/angularNestJSLambda/backend/src/book/book.resolver.ts
@@ -14,32 +14,27 @@ export class BookResolver {
         return await this.bookService.findAll();
     }
 
-    // @Query(() => BookType)
-    // async getOne(@Args('id') id: number) {
-    //     return await this.bookService.find(id);
-    // }
-    //
-    // @Mutation(() => BookType)
-    // async createItem(@Args('input') input: CreateBookInput): Promise<Book> {
-    //     const book = new Book();
-    //     book.author = input.author;
-    //     book.title = input.title;
-    //     return await this.bookService.create(book);
-    // }
-    //
-    // @Mutation(() => BookType)
-    // async updateItem(
-    //     @Args('input') input: UpdateBookInput): Promise<Book> {
-    //     return await this.bookService.update(input);
-    // }
-    //
-    // @Mutation(() => BookType)
-    // async deleteItem(@Args('id') id: number): Promise<number> {
-    //     return await this.bookService.delete(id);
-    // }
-    //
-    // @Query(() => String)
-    // async hello() {
-    //     return 'hello';
-    // }
+    @Query(() => BookType)
+    async getOne(@Args('id') id: number) {
+        return await this.bookService.find(id);
+    }
+
+    @Mutation(() => BookType)
+    async createItem(@Args('input') input: CreateBookInput): Promise<Book> {
+        const book = new Book();
+        book.author = input.author;
+        book.title = input.title;
+        return await this.bookService.create(book);
+    }
+
+    @Mutation(() => BookType)
+    async updateItem(
+        @Args('input') input: UpdateBookInput): Promise<Book> {
+        return await this.bookService.update(input);
+    }
+
+    @Mutation(() => BookType)
+    async deleteItem(@Args('id') id: number): Promise<number> {
+        return await this.bookService.delete(id);
+    }
 }
